bda01a1cc7f133dd8121680736dc5fc94db63e8a
JuanDevv21 <camilo20126@gmail.com>
2026-05-16 20:17:06 -0500

    Feature: PanelAdminDashboard

--- Dshop/src/pages/AdminDashboard/AdminDashboard.jsx
+++ Dshop/src/pages/AdminDashboard/AdminDashboard.jsx
@@ -7,6 +7,12 @@ import Clover from "../../assets/svg/clover.svg"
 import helpCircle from "../../assets/svg/help-circle.svg"
 import Gear from "../../assets/svg/gear.svg"
 import OrderTrendsChart from "./Grafico";
+import AlertCircle from "../../assets/svg/alert-circle.svg"
+import CheckCircle from "../../assets/svg/check-circle.svg"
+import Plane from "../../assets/svg/plane.svg"
+import TrasureChest from "../../assets/svg/treasure-chest.svg"
+import UserBlue from "../../assets/svg/userblue.svg"
+
 
 const AdminDashboard = () => {
     return (
@@ -62,15 +68,110 @@ const AdminDashboard = () => {
                         <p>3 items requieren revision urgente</p>
                     </div>
                 </div>
-                <div className="menu-d2">
-                    <div>
-
+                <div className="menu-der2">
+                    <div className="menu-dizq">
+                        <div className="menu-dizq1">
+                            <div className="md1">
+                                <p>ANALITICAS DE RENDIMIENTO</p>
+                                <h2>Volumen de ordenes</h2>
+                            </div>
+                            <div className="yu">
+                                <p className="ac">SEMANALMENTE</p>
+                                <p>MENSUALMENTE</p>
+                            </div>
+                        </div>
+                        <div className="menu-dizq2">
+                            <OrderTrendsChart></OrderTrendsChart>
+                        </div>
                     </div>
-                    <div>
-                        <OrderTrendsChart></OrderTrendsChart>
+
+                    <div className="menu-dder">
+                        <div className="fae">
+                            <p>ACTIVIDAD RECIENTE</p>
+                        </div>
+                        <div className="logisticabloq">
+                            <div>
+                                <img src={Plane}></img>
+                            </div>
+                            <div className="inf">
+                                <p>Pedido DS-9423 enviado</p>
+                            <span>Orlando, Puerto Cañaveral</span>
+                            </div>
+                        </div>
+                        <div className="logisticabloq">
+                            <div>
+                                <img src={AlertCircle}></img>
+                            </div>
+                            <div className="inf">
+                                <p>Paquete retenido - ID: DS-1010</p>
+                            <span>JFK Nueva York</span>
+                            </div>
+                        </div>
+                        <div className="logisticabloq"> 
+                            <div>
+                                <img src={UserBlue}></img>
+                            </div>
+                            <div className="inf">
+                                <p>Nuevo miembro de logistica agregado</p>
+                                <span>Sistema de seguridad - Hace 6 horas</span>
+                            </div>
+                        </div>
+                        <div className="logisticabloq">
+                            <div>
+                                <img src={CheckCircle}></img>
+                            </div>
+                            <div className="inf">
+                                <p>Restock de inventario finalizado</p>
+                            <span>Bodega de Buga, Valle del Cauca</span>
+                            </div>
+                        </div>
+                        <div className="logisticabloq">
+                            <div>
+                                <img src={TrasureChest}></img>
+                            </div>
+                            <div className="inf">
+                                <p>Alerta de alta demanda</p>
+                                <span>AirForce One</span>
+                            </div>
+                        </div>
+                        <div className="fae">
+                                <p>VER TODA LA ACTIVIDAD</p>
+                        </div>
                     </div>
                 </div>
-                <div className="menu-d3">Hola</div>
+                <div className="menu-der3">
+                    <div>
+                        <h3>LIVE GLOBAL SHIPMENTS</h3>
+                    </div>
+                    <div className="it">
+                        <p>TRACKING ID</p>
+                        <p>DESTINO</p>
+                        <p>ESTADO</p>
+                        <p>T. ESTIMADO ENTREGA</p>
+                        <p>PRIORIDAD</p>
+                    </div>
+                    <div className="it">
+                        <span style={{color: "blue"}}>#DS-9231</span>
+                        <span>Bogota</span>
+                        <span>Transitando</span>
+                        <span>Diciembre 12, 2026</span>
+                        <span>Ninguna</span>
+                    </div>
+                    <div className="it">
+                        <span style={{color: "blue"}}>#DS-0011</span>
+                        <span>Medellin</span>
+                        <span>Retrasado</span>
+                        <span>Diciembre 24, 2026</span>
+                        <span>Retraso</span>
+                    </div>
+                    <div className="it">
+                        <span style={{color: "blue"}}>#DS-6044</span>
+                        <span>Bogota</span>
+                        <span>Almacenado</span>
+                        <span>Diciembre 07, 2026</span>
+                        <span>Ninguna</span>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- Dshop/src/pages/AdminDashboard/Grafico.jsx
+++ Dshop/src/pages/AdminDashboard/Grafico.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, Cell } from 'recharts';
 
 const data = [
-  { name: 'MON', value: 400 },
-  { name: 'TUE', value: 680, active: true }, // Este tendrá el tooltip
-  { name: 'WED', value: 450 },
-  { name: 'THU', value: 850, highlighted: true }, // El más oscuro
-  { name: 'FRI', value: 300 },
-  { name: 'SAT', value: 550 },
-  { name: 'SUN', value: 480 },
+    { name: 'LUNES', value: 400 },
+    { name: 'MARTES', value: 680, active: true }, // Este tendrá el tooltip
+    { name: 'MIERCOLES', value: 450 },
+    { name: 'JUEVES', value: 850, highlighted: true }, // El más oscuro
+    { name: 'VIERNES', value: 300 },
+    { name: 'SABADO', value: 550 },
+    { name: 'DOMINGO', value: 480 },
 ];
 
 const CustomTooltip = ({ active, payload }) => {
@@ -24,20 +24,19 @@ const CustomTooltip = ({ active, payload }) => {
 
 const OrderTrendsChart = () => {
     return (
-    <div style={{ width: '100%', height: 300, background: '#fff', padding: '20px' }}>
+    <div style={{ width: '95%', height: 300, background: '#fff', padding: '20px' }}>
         <ResponsiveContainer width="100%" height="100%">
         <BarChart data={data} margin={{ top: 40, right: 0, left: 0, bottom: 0 }}>
             <XAxis 
             dataKey="name" 
             axisLine={false} 
             tickLine={false} 
-            tick={{ fill: '#999', fontSize: 12 }} 
-            dy={15}
+            tick={{ fill: '#999', fontSize: 16 }} 
+            dy={10}
             />
             <Tooltip 
             content={<CustomTooltip />} 
             cursor={{ fill: 'transparent' }} 
-            // Esto hace que el tooltip solo aparezca en el que marcamos como active
             active={true} 
             />
             <Bar dataKey="value" radius={[2, 2, 0, 0]}>
